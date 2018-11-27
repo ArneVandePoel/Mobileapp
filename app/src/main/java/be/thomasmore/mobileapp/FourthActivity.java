0b066f5b5c5eb143e955afaa4c4df966580a9b65
CodingDaveS <dave.saenen@hotmail.com>
2018-11-27 14:02:14 +0100

    schermen

---
+++ app/src/main/java/be/thomasmore/mobileapp/FourthActivity.java
@@ -0,0 +1,26 @@
+package be.thomasmore.mobileapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class FourthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    public void GaNaarSecondActivity(View v)
+    {
+        Intent intent= new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+}
--- app/src/main/java/be/thomasmore/mobileapp/MainActivity.java
+++ app/src/main/java/be/thomasmore/mobileapp/MainActivity.java
@@ -1,5 +1,6 @@
 package be.thomasmore.mobileapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,14 +19,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -48,5 +41,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void GaNaarSecondActivity(View v)
+    {
+        Intent intent= new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
