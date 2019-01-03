ae619c29ff3f9645cc45330b416c0672394b476d
ArneVandePoel <r0655272@student.thomasmore.be>
2019-01-03 14:56:31 +0100

    zeg op varkentjes klikken werkt

--- app/src/main/java/be/thomasmore/mobileapp/ZegActivity.java
+++ app/src/main/java/be/thomasmore/mobileapp/ZegActivity.java
@@ -1,15 +1,23 @@
 package be.thomasmore.mobileapp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class ZegActivity extends AppCompatActivity {
 
     String woord1;
     String woord2;
     String[] woorden;
+    String woord;
+    MediaPlayer mediaPlayer;
+    String tag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +40,35 @@ public class ZegActivity extends AppCompatActivity {
             ImageView imageView = (ImageView)findViewById(getResources().getIdentifier("prent" + i, "id", getPackageName()));
             imageView.setImageResource(R.drawable.varken);
         }
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.spel3);
+        mediaPlayer.start();
     }
 
+    public void Zeg(View v){
+        stopPlaying();
+        int random = new Random().nextInt(2);
+        woord = woorden[random];
+        int geluid = getResources().getIdentifier(woord, "raw", getPackageName());
+        mediaPlayer = MediaPlayer.create(this, geluid);
+        mediaPlayer.start();
+        tag = v.getTag() + "";
+        ImageView imageView = (ImageView)findViewById(getResources().getIdentifier(tag, "id", getPackageName()));
+        imageView.setImageResource(getResources().getIdentifier(woord, "drawable", getPackageName()));
+    }
 
+    public void onPause()
+    {
+        super.onPause();
+        stopPlaying();
+
+    }
+
+    private void stopPlaying() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 }
